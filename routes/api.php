89d8880f1a01d3209dbcf64c353c4d5adbd3ca0a
mkbcentral <mkbcentralcode@gmail.com>
2023-01-18 08:02:58 +0200

    Reset mobile api

--- routes/api.php
+++ routes/api.php
@@ -13,19 +13,20 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|Route::controller(RecttesController::class)->group(function(){
-    Route::get('recettes-by-day/{date}','getRecttesByDay');
-    Route::get('recettes-by-month/{month}','getByMonth');
-    Route::get('recettes-minerval-by-month/{month}','getMinervalByMonth');
-    Route::get('recettes-minerval-by-day/{day}','getMinervalByDay');
-    Route::get('recettes-all/{month}','getAllRecettes');
-});
+|
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
 
+Route::controller(RecttesController::class)->group(function(){
+    Route::get('recettes-by-day/{date}','getRecttesByDay');
+    Route::get('recettes-by-month/{month}','getByMonth');
+    Route::get('recettes-minerval-by-month/{month}','getMinervalByMonth');
+    Route::get('recettes-minerval-by-day/{day}','getMinervalByDay');
+    Route::get('recettes-all/{month}','getAllRecettes');
+});
 
 Route::controller(AuthController::class)->group(function(){
     Route::post('login','login');
