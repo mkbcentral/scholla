fb43705bb3fc620ef56892fd7e9b12c9ed072895
mkbcentral <mkbcentralcode@gmail.com>
2023-02-24 07:59:16 +0200

    Make archive juin global 2

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -210,7 +210,7 @@
                          </x-nav-link>
                          <x-nav-link class="nav-link" href="{{ route('archive.juin.global') }}" :active="request()->routeIs('archive.juin.global')">
                             &#x1F5C3;
-                            Archive juin mensuel
+                            Archive juin global
                          </x-nav-link>
                         <x-nav-link class="nav-link" href="{{ route('recettes.index') }}" :active="request()->routeIs('recettes.index')">
                             &#x1F4C1;
